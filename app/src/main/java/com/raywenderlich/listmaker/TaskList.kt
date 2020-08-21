91f26e70eba16ccf21fb3f16d32b40c91410e0a0
Herve Desrosiers <hdesrosiers@stingray.com>
2020-08-21 07:54:19 -0400

    step 06. implemented Parcel interface; understood the theory only... implementation details got me lost

--- app/src/main/java/com/raywenderlich/listmaker/TaskList.kt
+++ app/src/main/java/com/raywenderlich/listmaker/TaskList.kt
@@ -1,4 +1,30 @@
 package com.raywenderlich.listmaker
 
-class TaskList(val name: String, val tasks: ArrayList<String> = ArrayList()) {
+import android.os.Parcel
+import android.os.Parcelable
+
+class TaskList(val name: String, val tasks: ArrayList<String> = ArrayList()): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!, //find and unwrap name of list
+        parcel.createStringArrayList()!! //find and unwrap the list
+    )
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        //convert task list into parcel
+        dest.writeString(name)
+        dest.writeStringList(tasks)
+    }
+
+    override fun describeContents() = 0 //no file descriptors
+
+    companion object CREATOR : Parcelable.Creator<TaskList> {
+        override fun createFromParcel(source: Parcel): TaskList {
+            return TaskList(source)
+        }
+
+        override fun newArray(size: Int): Array<TaskList?> {
+            return arrayOfNulls(size)
+        }
+    }
+
 }
